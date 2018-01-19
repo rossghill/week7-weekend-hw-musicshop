151dc7290e5b9e05481b941690f70a02fa8d0487
Ross Hill <rosshill88@gmail.com>
2018-01-19 19:00:37 +0000

    Created Instrument abstract superclass

---
+++ src/main/java/Instrument.java
@@ -0,0 +1,12 @@
+public abstract class Instrument {
+
+    String colour;
+    int neighbour_annoyance_level;
+
+    public Instrument( String colour, int neighbour_annoyance_level) {
+        this.colour = colour;
+        this.neighbour_annoyance_level = neighbour_annoyance_level;
+    }
+
+
+}
